881b9154f8060518dadd80a793b9a70042a76972
hatemodify <hatemodify@gmail.com>
2018-10-02 01:02:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const methodOverride = require('method-override');
+
+
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 9000);
+
+
+app.get('/', (req, res) => {
+  res.render('index.html', {
+    title: 'MAD',
+    arr: [1, 2, 3, 4, 5, 6, 7]
+  });
+  console.log(res);
+});
